8b9adab1e4cc04795aee519ae4d4cf1c62610e02
learning-coding10 <143152528+learning-coding10@users.noreply.github.com>
2024-12-25 21:24:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ elif st.session_state['page'] == 'chat':
         st.markdown(
             f"""
             <div style="
-                background-color: #78bae4; 
+                background-color: #40a4e2; 
                 padding: 10px;
                 color: #fff;
                 border-radius: 10px; 
@@ -190,7 +190,7 @@ elif st.session_state['page'] == 'chat':
         st.markdown(
             f"""
             <div style="
-                background-color:  #D3D3D3; 
+                background-color:  #aaa8a8; 
                 padding: 10px; 
                 color: #fff;
                 border-radius: 10px; 
